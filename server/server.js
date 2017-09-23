1c672ed0b800198324a6a4d4455d38c223c40d7d
sreemukha <sreemukha.mail@gmail.com>
2017-09-22 20:30:51 -0500

    Set up Test and Develpment Envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -11,7 +13,7 @@ const {User} = require('./models/user');
 
 //create express app
 let app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT;
 
 
 
