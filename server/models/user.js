1478c49a817c352b0a58902b66a027477f24fa1b
sreemukha <sreemukha.mail@gmail.com>
2017-09-24 16:48:43 -0500

    Added token methods

--- server/models/user.js
+++ server/models/user.js
@@ -1,37 +1,72 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 
-let User = mongoose.model('User', {
-  email:{
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true,
-    unique: true,
-    validate: {
-      validator: (value) => {
-          return validator.isEmail(value);
-      },
-      message: '{VALUE} is an invalid email'
-    }
-  },
-  password:{
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  tokens:[{
-    access:{
+let UserSchema = new mongoose.Schema({
+    email:{
       type: String,
-      required: true
+      required: true,
+      minlength: 1,
+      trim: true,
+      unique: true,
+      validate: {
+        validator: (value) => {
+            return validator.isEmail(value);
+        },
+        message: '{VALUE} is an invalid email'
+      }
     },
-    token:{
+    password:{
       type: String,
-      required: true
-    }
-  }]
+      required: true,
+      minlength: 6
+    },
+    tokens:[{
+      access:{
+        type: String,
+        required: true
+      },
+      token:{
+        type: String,
+        required: true
+      }
+    }]
 });
+
+// Instance methods on the User model
+
+// not arrow function because 'this' keyword is required here
+// arrow functions do not bind 'this' keyword
+
+// overriding toJSON method
+UserSchema.methods.toJSON = function(){
+  let user = this;
+  let userObject = user.toObject(); // mongoose variable 'user' to regular object
+  return _.pick(userObject,['_id','email']);
+}
+
+UserSchema.methods.generateAuthToken = function(){
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({_id: user._id.toHexString(), access: access}, 'secret').toString();
+    // ES5 syntax without destructuring
+    // user.tokens.push({
+    //   access:access,
+    //   token:token
+    // });
+
+    // ES6 syntax with destructuring
+    user.tokens.push({access, token});
+
+    // this will be a success for promise in server.js
+    return user.save().then(() => {
+      return token;
+    });
+};
+
+let User = mongoose.model('User', UserSchema);
 
 
 
--- server/server.js
+++ server/server.js
@@ -114,9 +114,11 @@ app.post('/users', (req,res) => {
   const body = _.pick(req.body, ['email','password']);
   const user = new User(body);
 
-  user.save().then((user) => {
-    res.status(200).send(user);
-  }, (err) => {
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth',token).send(user);
+  }).catch((err) => {
     res.status(400).send(err);
   })
 
