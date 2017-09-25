2d646496668cb82858a2ca5594909eb32ca21e8f
sreemukha <sreemukha.mail@gmail.com>
2017-09-25 14:46:49 -0500

    Added POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 
 // local imports
@@ -142,6 +143,21 @@ app.get('/users/me', authenticate, (req,res) => {
   //   // 401 - authentication required
   //   res.status(401).send();
   // });
+});
+
+// Configuring private POST /users/login {email, password} route
+
+app.post('/users/login', (req,res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email,body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(port, ()=>{
