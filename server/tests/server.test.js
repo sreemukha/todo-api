289fc49cdcf35f0960d97abb144cd599effc0a31
sreemukha <sreemukha.mail@gmail.com>
2017-09-25 15:01:19 -0500

    Added tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -227,7 +227,7 @@ describe('POST /users', () => {
           expect(user).toExist();
           expect(user.password).toNotBe(pwd);
           done();
-        });
+        }).catch((e) => done(e));
       });
   });
 
@@ -249,5 +249,51 @@ describe('POST /users', () => {
       .send({email:users[1].email, password})
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return x-auth token', (done) => {
+
+    request(app)
+      .post('/users/login')
+      .send({email:users[1].email,password:users[1].password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          });
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should reject invalid reject', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({email:users[1].email,password:'invalid!'})
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
   });
 });
