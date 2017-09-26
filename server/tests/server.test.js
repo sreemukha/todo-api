0c02581fba3a0a0f2147aaf4bd0278e9a96245cb
sreemukha <sreemukha.mail@gmail.com>
2017-09-26 18:09:58 -0500

    Added tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -285,12 +285,30 @@ describe('POST /users/login', () => {
       .expect((res) => {
         expect(res.headers['x-auth']).toNotExist();
       })
-      .end((err, res) => {
+      .end((err) => {
         if(err){
           return done(err);
         }
 
         User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should log out the user by removing auth token', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth',users[0].tokens[0].token)
+      .expect(200)
+      .end((err) => {
+        if(err){
+          return done(err);
+        }
+        User.findById(users[0]._id).then((user) => {
           expect(user.tokens.length).toBe(0);
           done();
         }).catch((e) => done(e));
